5e56855e115e06c555c1c5b51d821ac8a125c65d
EDICARLOS <edicarlosraiox@gmail.com>
2022-04-27 13:14:21 -0300

    Create media idades.cs

---
+++ Media idades.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         Console.WriteLine("Digite o valor 1");
+        decimal valor1 = Console.Read();
+        Console.WriteLine("Digite o valor 2;");
+        decimal valor2 = Console.ReadLine();
+
+        decimal media = (valor1+valor2)/2.0M;
+
+        Console.WriteLine($"A média é:  {media}");
+    }
+}
