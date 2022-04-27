3c663229111aed758c60f7a6be88b24b70c4f53e
EDICARLOS <edicarlosraiox@gmail.com>
2022-04-27 13:48:30 -0300

    Exercício variáveis na pasta média idades

---
+++ Ecercicio variaveis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace program 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int x , y;
+            x = 5;
+            y = 2 * x;
+            Console.WriteLine( x );
+            Console.WriteLine(y);
+        
+            
+        }
+    }
+}
+
+
