181592c9ca86747a716208b92bb1127086e95984
EDICARLOS <104223057+edicarlosraiox@users.noreply.github.com>
2022-04-29 01:51:42 -0300

    Exercício

---
+++ CSharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Matrizes
+{
+    class Matrizes
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int M, N;
+            Console.Write(" Quantas linhas vai ter a matriz ?: ");
+            M = int.Parse(Console.ReadLine());
+            Console.Write(" Quantas colunas vai ter a matriz ?: ");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(" Elemento [" + i + "," + j + "]: ");
+                    mat = [i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(" Matriz digitada: ");
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(mat[i, j] + " ");
+
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
+
---
+++ CSharp/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double largura, altura, comprimento, perimetro, area, diagonal;
+
+            Console.Write(" Digite a base do retngulo :");
+            largura = double.Parse(Console.ReadLine( ), CI);
+            Console.Write(" Digite a altura do retâgulo :");
+            altura = double.Parse(Console.ReadLine( ), CI);
+
+            area = largura * altura;
+            perimetro = 2 * (largura + altura);
+            diagonal = Math.Sqrt(Math.Pow(largura, 2.0) + Math.Pow(altura, 2.0));
+
+            Console.WriteLine(" Area = " + area.ToString("F4"), CI);
+            Console.WriteLine(" Perimetro = " + perimetro.ToString("F4"), CI);
+            Console.WriteLine(" Diagonal = " + diagonal.ToString("F4"), CI);
+
+
+        }
+    }
+}
