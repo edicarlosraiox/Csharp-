68bcb284eccdc8cb071383eccc0b486b686dbe4a
EDICARLOS <edicarlosraiox@gmail.com>
2022-04-29 23:39:15 -0300

    Estrutura for

---
+++ CSharp/Estrutura_para.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace Estrutura_Para
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int N, i, x, soma;
+
+            Console.Write(" Quantos numeros serao digitados? ");
+            N = int.Parse(Console.ReadLine(), CI);
+
+            soma = 0;
+            for (  i = 1; i <  N; i++)
+            {
+                Console.Write(" Digite um numero: ");
+                x = int.Parse(Console.ReadLine(),CI);
+                soma = soma + x;
+            }
+            Console.WriteLine(" SOMA = " + soma.ToString("F1", CI));
+        }
+    }
+}
