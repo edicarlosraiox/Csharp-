dfee02fc4c36a427096f602bd6545517900e84af
EDICARLOS <edicarlosraiox@gmail.com>
2022-05-04 19:22:15 -0300

    Create Estrutura_condicional.cs

---
+++ CSharp/Estrutura_condicional.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Condição
+{ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int hora;
+
+        Console.Write(" Digite uma hora do dia: ");
+        hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12) 
+        {
+            Console.WriteLine(" Bom Dia!");
+        }
+
+        else if (hora > 18) 
+        {
+            Console.WriteLine(" Boa Noite!");
+        }
+
+        else
+        {
+            Console.WriteLine(" Boa Tarde !");
+        }
+        
+      }
+
+   }
+}
+ 
+
+   
+
+      
+  
+
+
+
+
+
