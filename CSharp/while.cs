1b88af381a847f53661a0af7e24d86bf7fdce47e
EDICARLOS <edicarlosraiox@gmail.com>
2022-05-04 17:30:33 -0300

    Creat while2.cs

---
+++ CSharp/while.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, soma;
+
+            soma = 0;
+            Console.Write(" Digite o primeiro numero: ");
+            x = int.Parse(Console.ReadLine());  
+            
+            while (x!= 0) 
+            {
+                soma = soma + x;
+                Console.Write(" Digite outro numero: ");
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(" Soma = " + soma);
+        }
+    }
+}
+
+
+
+
