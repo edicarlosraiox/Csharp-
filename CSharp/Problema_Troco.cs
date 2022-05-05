aa6243bcbb99a6f1ecbc18318b2a019c85f33bf4
EDICARLOS <edicarlosraiox@gmail.com>
2022-05-05 14:16:29 -0300

    Problema_Troco

---
+++ CSharp/Problema_Troco.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problema_Troco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double troco, dinheiro,preco;
+            int quantidade;
+
+            Console.Write(" Preço unitario do prduto: ");
+            preco = double.Parse(Console.ReadLine(),CI);
+            Console.Write(" Quantidade comprada: ");
+            quantidade = int.Parse(Console.ReadLine(),CI);
+            Console.Write(" Dinheiro recebido: ");
+            dinheiro = double.Parse(Console.ReadLine(),CI);
+
+            troco = dinheiro - (quantidade * preco);
+
+            Console.WriteLine();
+            Console.WriteLine(" DADOS ");
+            Console.WriteLine(" Preço: " + preco.ToString("F2",CI));
+            Console.WriteLine(" Quantidade: " + quantidade);
+            Console.WriteLine(" Dinheiro " + dinheiro.ToString("F2", CI));
+            Console.WriteLine(" Troco: " + troco.ToString("F2", CI));
+        }
+    }
+}
+
---
+++ Estrutura_para.cs
@@ -0,0 +1,5 @@
+﻿Build iniciado...
+1>------ Compilação iniciada: Projeto: ConsoleApp22, Configuração: Debug Any CPU ------
+1>Ignorando analisadores para acelerar a construção. Você pode executar o comando 'Build' ou 'Rebuild' para executar os analisadores.
+1>ConsoleApp22 -> C:\Users\Edicarlos\source\repos\ConsoleApp22\bin\Debug\net6.0\ConsoleApp22.dll
+========== Compilar: 1 com êxito, 0 com falha, 0 atualizados, 0 ignorados ==========
