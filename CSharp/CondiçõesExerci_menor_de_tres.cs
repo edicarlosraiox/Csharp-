2523cd74a2115d02a574a54b0eef40f6f9b7b7b6
EDICARLOS <edicarlosraiox@gmail.com>
2022-05-05 20:26:03 -0300

    Condições_exercicio_menor_de_tres
    problema menor de três

---
+++ CSharp/CondiçõesExerci_menor_de_tres.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;  
+
+namespace Menor_de_tres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int a, b, c, menor;
+
+            Console.Write(" Primeiro Valor:");
+            a = int.Parse(Console.ReadLine(), CI);
+            Console.Write(" Segundo valor: ");
+            b = int.Parse(Console.ReadLine(), CI);
+            Console.Write(" Terceiro valor:  ");
+            c = int.Parse(Console.ReadLine(), CI);
+
+            if (a < b && a < c) {
+                menor = a;
+            }
+            else if(b < c) {
+                menor = b;
+            }
+            else{
+                menor = c;
+            }
+            Console.WriteLine(" Menor = " + menor);
+        }
+    }
+}
