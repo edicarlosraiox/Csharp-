9a992cd44075a97568024eb966062b93a1c0d73b
EDICARLOS <edicarlosraiox@gmail.com>
2022-05-04 21:21:52 -0300

    Create RaizQuadrada.cs

---
+++ CSharp/RaizQuadrada.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace raiz_quadrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            
+
+            int x;
+
+            Console.WriteLine("Digite um numero:");
+            x = int.Parse(Console.ReadLine());
+
+            double raiz_quadrada = Math.Sqrt(x);
+            Console.WriteLine("Resultado: " + raiz_quadrada);
+        }
+        
+    }
+    
+}
